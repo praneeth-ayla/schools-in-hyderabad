0555eddabe4482e810f78cd8308a8343fdc28739
Praneeth <praneethayla@gmail.com>
2024-08-11 00:57:29 +0530

    school page

---
+++ src/components/Carousel.tsx
@@ -0,0 +1,46 @@
+"use client";
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
+
+export function CarouselSpacing({ images }: { images: string[] }) {
+	return (
+		<Carousel
+			plugins={[
+				Autoplay({
+					delay: 2000,
+				}),
+			]}>
+			<CarouselContent className="w-full h-[150px] sm:h-[250px] md:h-[350px] lg:h-[500px]">
+				{images.map((e, i) => (
+					<CarouselItem
+						key={i}
+						className="w-full h-full">
+						<div
+							className="flex justify-center items-center"
+							style={{
+								overflow: "hidden",
+								display: "flex",
+								justifyContent: "center",
+								alignItems: "center",
+								width: "100%",
+								height: "100%", // Set height to 100%
+							}}>
+							<img
+								className="w-full h-full object-cover"
+								src={e}
+								alt={`image ${i + 1}`}
+								style={{
+									objectPosition: "center",
+								}}
+							/>
+						</div>
+					</CarouselItem>
+				))}
+			</CarouselContent>
+		</Carousel>
+	);
+}
