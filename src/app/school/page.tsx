abdbb71bafbcff034cf6700ccc815b3377b24757
Praneeth <praneethayla@gmail.com>
2024-08-13 00:36:58 +0530

    minor changes

--- src/app/school/page.tsx
+++ src/app/school/page.tsx
@@ -8,6 +8,7 @@ import { CardDescription, CardTitle } from "@/components/ui/card";
 import { useSchoolDetails } from "../../lib/hooks";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import Loading from "@/components/Loading";
 
 export default function page({ searchParams }: any) {
 	const schoolId = searchParams.id;
@@ -17,101 +18,12 @@ export default function page({ searchParams }: any) {
 	useEffect(() => {
 		if (failed) router.back();
 	}, [failed]);
-
-	if (isLoading)
-		return (
-			<div className="flex justify-center items-center h-screen">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					className="h-10 w-auto text-blue-300"
-					viewBox="0 0 24 24">
-					<g stroke="currentColor">
-						<circle
-							cx="12"
-							cy="12"
-							r="9.5"
-							fill="none"
-							stroke-linecap="round"
-							stroke-width="3">
-							<animate
-								attributeName="stroke-dasharray"
-								calcMode="spline"
-								dur="1.5s"
-								keySplines="0.42,0,0.58,1;0.42,0,0.58,1;0.42,0,0.58,1"
-								keyTimes="0;0.475;0.95;1"
-								repeatCount="indefinite"
-								values="0 150;42 150;42 150;42 150"
-							/>
-							<animate
-								attributeName="stroke-dashoffset"
-								calcMode="spline"
-								dur="1.5s"
-								keySplines="0.42,0,0.58,1;0.42,0,0.58,1;0.42,0,0.58,1"
-								keyTimes="0;0.475;0.95;1"
-								repeatCount="indefinite"
-								values="0;-16;-59;-59"
-							/>
-						</circle>
-						<animateTransform
-							attributeName="transform"
-							dur="2s"
-							repeatCount="indefinite"
-							type="rotate"
-							values="0 12 12;360 12 12"
-						/>
-					</g>
-				</svg>
-			</div>
-		);
-	if (!details)
-		return (
-			<div className="flex justify-center items-center h-screen">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					className="h-10 w-auto text-blue-300"
-					viewBox="0 0 24 24">
-					<g stroke="currentColor">
-						<circle
-							cx="12"
-							cy="12"
-							r="9.5"
-							fill="none"
-							stroke-linecap="round"
-							stroke-width="3">
-							<animate
-								attributeName="stroke-dasharray"
-								calcMode="spline"
-								dur="1.5s"
-								keySplines="0.42,0,0.58,1;0.42,0,0.58,1;0.42,0,0.58,1"
-								keyTimes="0;0.475;0.95;1"
-								repeatCount="indefinite"
-								values="0 150;42 150;42 150;42 150"
-							/>
-							<animate
-								attributeName="stroke-dashoffset"
-								calcMode="spline"
-								dur="1.5s"
-								keySplines="0.42,0,0.58,1;0.42,0,0.58,1;0.42,0,0.58,1"
-								keyTimes="0;0.475;0.95;1"
-								repeatCount="indefinite"
-								values="0;-16;-59;-59"
-							/>
-						</circle>
-						<animateTransform
-							attributeName="transform"
-							dur="2s"
-							repeatCount="indefinite"
-							type="rotate"
-							values="0 12 12;360 12 12"
-						/>
-					</g>
-				</svg>
-			</div>
-		);
+	if (isLoading) return <Loading />;
+	if (!details) return <Loading />;
 
 	return (
 		<div className="sm:px-4 md:px-10 lg:px-20 xl:px-52 pt-4 lg:pt-8">
-			<p className="text-center pb-3 font-bold text-muted-foreground text-lg md:text-xl">
+			<p className="text-center pb-3 px-2 font-bold text-muted-foreground text-lg md:text-xl">
 				{details.name}
 			</p>
 			<div className="grid lg:flex lg:flex-row">
