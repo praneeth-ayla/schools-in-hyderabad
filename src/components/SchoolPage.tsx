4b2cc348a3241dcfcb4026368e52d6e3b1bfa965
S. Adithiya <adithiya1805@gmail.com>
2024-08-24 17:13:39 +0530

    Fon-style added

--- src/components/SchoolPage.tsx
+++ src/components/SchoolPage.tsx
@@ -230,6 +230,7 @@ export default function SchoolPage({ schoolId }: { schoolId: string }) {
 											</p>
 											<div className="flex gap-3 flex-wrap">
 												<TextGenerateEffect
+												className="head font-bold"
 													words={details.facilities
 														.map(
 															// @ts-ignore
