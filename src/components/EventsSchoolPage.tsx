85fafc8bb4002788f7770dfe18c68e5775801ab9
Praneeth <praneethayla@gmail.com>
2024-09-15 16:07:26 +0530

    DateTime

--- src/components/EventsSchoolPage.tsx
+++ src/components/EventsSchoolPage.tsx
@@ -48,11 +48,11 @@ export default function EventsSchoolPage({
 												</div>
 												<div className="mt-auto">
 													<CardTitle className="flex flex-col gap-3 pb-3 pt-1">
-														<p className="text-sm font-normal">
+														{/* <p className="text-sm font-normal">
 															{DateTimeDisplay(
 																event.date
 															).slice(0, 17)}
-														</p>
+														</p> */}
 														<span>
 															{event.title}
 														</span>
@@ -109,11 +109,11 @@ export default function EventsSchoolPage({
 												</div>
 												<div className="mt-auto">
 													<CardTitle className="flex flex-col gap-3 pb-3 pt-1">
-														<p className="text-sm font-normal">
+														{/* <p className="text-sm font-normal">
 															{DateTimeDisplay(
 																event.date
 															).slice(0, 17)}
-														</p>
+														</p> */}
 														<span>
 															{event.title}
 														</span>
